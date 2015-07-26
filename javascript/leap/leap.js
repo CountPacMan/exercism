3fc6abc3bb8135b3225f4940c89b49e81f165a55
Daniel Toader <daniel.t.pdx@gmail.com>
2015-07-25 22:07:17 -0700

    change to triple equals test

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,10 +1,10 @@
 module.exports = function(input) {
   // year is evenly divisible by 4
-  var dBy4 = (input % 4 == 0);
+  var dBy4 = (input % 4 === 0);
   // year is evenly divisible by 100
-  var dBy100 = (input % 100 == 0);
+  var dBy100 = (input % 100 === 0);
   // year is evenly divisible by 400
-  var dBy400 = (input % 400 == 0);
+  var dBy400 = (input % 400 === 0);
 
   // leap year if input is evenly divisible by 4 but not by 100 unless also
   // evenly divisible by 400
