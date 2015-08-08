6445dd74432a6ad34d53663264abf3d17196e899
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-08 13:03:08 -0700

    add logic to swap keys and values

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,3 +1,12 @@
-module.exports = function() {
-
+module.exports = function(input) {
+  var output = {};
+  for (var key in input) {
+    console.log('key = ' + key);
+    for (var i in input[key]) {
+      var value = input[key][i].toLowerCase();
+      console.log('value = ' + value);
+      output[value] = parseInt(key);
+    }
+  }
+  return output;
 };
