abb5c15e4d063e106c287f1dfe991571de301704
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-08 14:17:43 -0700

    remove console logs

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,10 +1,8 @@
 module.exports = function(input) {
   var output = {};
   for (var key in input) {
-    console.log('key = ' + key);
     for (var i in input[key]) {
       var value = input[key][i].toLowerCase();
-      console.log('value = ' + value);
       output[value] = parseInt(key);
     }
   }
