d21b293d30a0243308107b84c678d4c5dfaa308b
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-08 20:08:36 -0700

    refactor

--- php/leap/leap.php
+++ php/leap/leap.php
@@ -1,9 +1,14 @@
 <?php
 
 function isLeap($input) {
-  if ($input % 100 === 0 && $input % 400 != 0 || $input % 4 != 0) {
-    return false;
-  } else {
-    return true;
-  }
+  // year is evenly divisible by 4
+  $divBy4 = $input % 4 === 0;
+  // year is evenly divisible by 100
+  $divBy100 = $input % 100 === 0;
+  // year is evenly divisible by 400
+  $divBy400 = $input % 400 === 0;
+
+  // leap year if input is evenly divisible by 4 but not by 100 unless also
+  // evenly divisible by 400
+  return $divBy100 ? $divBy400 : $divBy4;
 }
