c1f37712980a54ccea3aa10f44a8ecd9448eba53
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-08 21:22:25 -0700

    add test for nonvalid characters

--- php/trinary/trinary.php
+++ php/trinary/trinary.php
@@ -1,9 +1,13 @@
 <?php
 
 function toDecimal($input) {
+  // all numbers must be 0, 1, or 2
+  if (preg_match('/[^0-2]/', $input)) {
+    return 0;
+  }
   // split input
   $input = str_split($input);
-  // foreach += #*3^array element number
+  // reverse foreach += #*3^array element number
   $output = 0;
   for ($length = count($input), $i = $length - 1; $i >= 0; $i--) {
     $output += $input[$i] * pow(3, $length - $i - 1);
