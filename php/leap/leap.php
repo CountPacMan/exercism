b8b0a465cd265be110c499585569142872b4ce58
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-08 19:49:25 -0700

    refactor

--- php/leap/leap.php
+++ php/leap/leap.php
@@ -1,15 +1,9 @@
 <?php
 
 function isLeap($input) {
-  if ($input % 4 === 0) {
-    if ($input % 100 === 0 && $input % 400 === 0) {
-      return true;
-    } elseif ($input % 100 === 0) {
-      return false;
-    } else {
-      return true;
-    }
-  } else {
+  if ($input % 100 === 0 && $input % 400 != 0 || $input % 4 != 0) {
     return false;
+  } else {
+    return true;
   }
 }
