e6472fc1c10c3d532b78ad044a0e97447593bb95
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-19 22:02:12 -0700

    add more logic

--- php/bowling/bowling.php
+++ php/bowling/bowling.php
@@ -1,21 +1,37 @@
 <?php
 
 class Game {
-  private $score;
   private $frames;
   private $throw;
 
   function __construct() {
-    $this->score = 0;
     $this->frames = [];
     $this->throw = 1;
   }
 
   function roll($pins) {
-    // test for strike
-    if ($this->score > )
+    $current = count($this->frames);
+    if ($current > 2) {
+      // test for previous strike that is ready to count
+      if ($this->frames[$current - 2] === 10) {
+        $this->frames[] = $this->frames[$current - 2] + $this->frames[$current - 1] + $pins;
+      }
+      // test for previous strike not ready to count
+      elseif ($this->frames[$current - 1] === 10) {
+        $this->frames[] = $pins;
+      }
+      // test for previous spare
+      elseif ($this->throw === 2 && $this->frames[$current - 1] + $pins === 10) {
+        $this->score += $pins;
+      }
+    } elseif ($current === 1) {
+      if ($this->frames[1] === 10) {
+        // strike
+      } elseif ($this->frames[])
+    }
 
-    echo "throw = " . ($this->throw) . "\n";
+
+    echo "throw = " . $this->throw . "\n";
     echo "score = " . $this->score . "\n";
   }
 
