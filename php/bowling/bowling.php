c3462537c0b39cb4568142e545687c936eb5b6be
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-19 22:32:13 -0700

    refactor basic logic

--- php/bowling/bowling.php
+++ php/bowling/bowling.php
@@ -13,21 +13,22 @@ class Game {
     $current = count($this->frames);
     if ($current > 2) {
       // test for previous strike that is ready to count
-      if ($this->frames[$current - 2] === 10) {
-        $this->frames[] = $this->frames[$current - 2] + $this->frames[$current - 1] + $pins;
+      if ($this->frames[$current - 2] === 'X') {
+        $this->frames[$current - 2] = 10 + $this->frames[$current - 1] + $pins;
+        $this->frames[] = $pins;
       }
       // test for previous strike not ready to count
-      elseif ($this->frames[$current - 1] === 10) {
+      elseif ($this->frames[$current - 1] === 'X') {
         $this->frames[] = $pins;
       }
       // test for previous spare
       elseif ($this->throw === 2 && $this->frames[$current - 1] + $pins === 10) {
-        $this->score += $pins;
+        $this->frames[] = $pins;
       }
     } elseif ($current === 1) {
       if ($this->frames[1] === 10) {
         // strike
-      } elseif ($this->frames[])
+      } elseif ($this->frames[] )
     }
 
 
