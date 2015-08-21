fe737d2884a60fea60a12c859855253aeca994f7
Daniel Toader <daniel.t.pdx@gmail.com>
2015-08-20 19:10:47 -0700

    restart logic again

--- php/bowling/bowling.php
+++ php/bowling/bowling.php
@@ -1,36 +1,30 @@
 <?php
 
 class Game {
-  private $frames;
-  private $throw;
+  private $throws;
+  private $current;
 
   function __construct() {
-    $this->frames = [];
-    $this->throw = 1;
+    $this->throws = array();
+    $this->current = 0;
   }
 
   function roll($pins) {
-    $current = count($this->frames);
-    if ($current > 2) {
-      // test for previous strike that is ready to count
-      if ($this->frames[$current - 2] === 'X') {
-        $this->frames[$current - 2] = 10 + $this->frames[$current - 1] + $pins;
-        $this->frames[] = $pins;
-      }
-      // test for previous strike not ready to count
-      elseif ($this->frames[$current - 1] === 'X') {
-        $this->frames[] = $pins;
-      }
-      // test for previous spare
-      elseif ($this->throw === 2 && $this->frames[$current - 1] + $pins === 10) {
-        $this->frames[] = $pins;
-      }
-    } elseif ($current === 1) {
-      if ($this->frames[1] === 10) {
-        // strike
-      } elseif ($this->frames[] )
-    }
+    $this->throws[] = $pins;
+    $score = 0;
+    $frame = 0;
 
+    for ($i = 0; $i < 10; $i++) {
+      if($this->testStrike($frame)) {
+        $score += $this->throws[$frame + 1] + $this->throws[$frame + 2] + 10;
+        $frame++;
+      } elseif ($this->testSpare($frame)) {
+        $score += $this->throws[$frame + 1] + 10;
+        $frame += 2;
+      } else {
+        $score += $this->throws[$frame] + $this->throws[$frame + 1];
+      }
+    }
 
     echo "throw = " . $this->throw . "\n";
     echo "score = " . $this->score . "\n";
